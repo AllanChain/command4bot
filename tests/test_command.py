f9cf9cf05d3c7b2f3c6bb9d15931411262e89417
Allan Chain <allanchain@pku.edu.cn>
2020-11-08 17:40:55 +0800

    test: test setup

--- tests/test_command.py
+++ tests/test_command.py
@@ -3,6 +3,11 @@ import pytest
 from command4bot import CommandsManager
 
 
+@pytest.fixture(scope="class")
+def data_share():
+    return {}
+
+
 class TestBasicUsage:
     @pytest.fixture(scope="class")
     def mgr(self):
@@ -29,7 +34,7 @@ class TestBasicUsage:
         assert "world" in mgr.exec("word")
 
 
-class TestSetup:
+class TestBasicSetup:
     @pytest.fixture(scope="class")
     def mgr(self):
         mgr = CommandsManager()
@@ -50,6 +55,72 @@ class TestSetup:
 
     def test_setup(self, mgr):
         assert mgr.exec("send") == "abc"
+
+
+class TestGeneratorSetup:
+    @pytest.fixture(scope="class")
+    def mgr(self, data_share):
+        mgr = CommandsManager()
+        data_share["status"] = None
+
+        @mgr.setup
+        def data():
+            data_share["status"] = "pending"
+            yield "abc"
+            data_share["status"] = "done"
+
+        @mgr.command(default_closed=True)
+        def post(payload, data):
+            return data
+
+        return mgr
+
+    @pytest.fixture(scope="class")
+    def open_post(self, mgr):
+        mgr.open("post")
+
+    def test_not_inited_if_command_closed(
+        self, mgr: CommandsManager, data_share
+    ):
+        assert data_share["status"] is None
+        assert mgr.setup_reg.get("data").cached_generator is None
+        assert mgr.setup_reg.get("data").cached_value is None
+        assert not mgr.setup_reg.get("data").is_cached
+
+    def test_lazy_load(self, mgr, open_post, data_share):
+        assert data_share["status"] is None
+
+    def test_value_cached(self, mgr: CommandsManager, open_post):
+        assert mgr.exec("post") == "abc"
+        assert mgr.setup_reg.get("data").cached_generator
+        assert mgr.setup_reg.get("data").cached_value
+        assert mgr.setup_reg.get("data").is_cached
+
+
+class TestGeneratorSetupCleanup:
+    @pytest.fixture(scope="class")
+    def mgr(self, data_share):
+        mgr = CommandsManager()
+
+        @mgr.setup
+        def data():
+            data_share["status"] = "pending"
+            yield "abc"
+            data_share["status"] = "done"
+
+        @mgr.command
+        def post(payload, data):
+            return data
+
+        mgr.exec("post")
+        mgr.close("post")
+        return mgr
+
+    def test_cleanup(self, mgr: CommandsManager, data_share):
+        assert data_share["status"] == "done"
+        assert mgr.setup_reg.get("data").cached_generator is None
+        assert mgr.setup_reg.get("data").cached_value is None
+        assert not mgr.setup_reg.get("data").is_cached
 
 
 class TestOpenClose:

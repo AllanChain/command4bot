f242e0ebc7b5ec4fdbf6b99eed9e58d114198dc0
Allan Chain <allanchain@pku.edu.cn>
2020-11-22 08:58:07 +0800

    docs: fix sidebar width

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -47,6 +47,10 @@ exclude_patterns: List[str] = []
 #
 html_theme = "alabaster"
 
+html_theme_options = {
+    "sidebar_width": "250px",
+}
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".

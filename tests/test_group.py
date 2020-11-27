9963976302ae3d186ea68d8f283d0560e1405333
Allan <36528777+AllanChain@users.noreply.github.com>
2020-11-27 22:04:16 +0800

    test: add simple test for group

---
+++ tests/test_group.py
@@ -0,0 +1,58 @@
+from command4bot.command import CommandRegistry
+import pytest
+
+from command4bot import CommandsManager
+
+
+class TestGroupOpen:
+    @pytest.fixture(scope="class")
+    def mgr(self):
+        mgr = CommandsManager()
+
+        @mgr.command(groups=["hello"])
+        def hi():
+            return "hi"
+
+        @mgr.command(groups=["hello"])
+        def aloha():
+            return "aloha"
+
+        return mgr
+
+    @pytest.fixture(scope="class", autouse=True)
+    def close_group(self, mgr: CommandsManager):
+        mgr.close("hello")
+
+    def test_close_both(self, mgr):
+        assert mgr.exec("hi") == "CLOSED"
+        assert mgr.exec("aloha") == "CLOSED"
+
+
+class TestGroupOpen:
+    @pytest.fixture(scope="class")
+    def mgr(self):
+        command_reg = CommandRegistry()
+        mgr = CommandsManager(command_reg=command_reg)
+        command_reg.mark_default_closed("hello")
+
+        @mgr.command(groups=["hello"])
+        def hi():
+            return "hi"
+
+        @mgr.command(groups=["hello"])
+        def aloha():
+            return "aloha"
+
+        return mgr
+
+    @pytest.fixture(scope="class")
+    def open_group(self, mgr: CommandsManager):
+        mgr.open("hello")
+
+    def test_default_close(self, mgr):
+        assert mgr.exec("hi") == "CLOSED"
+        assert mgr.exec("aloha") == "CLOSED"
+
+    def test_open_both(self, mgr, open_group):
+        assert mgr.exec("hi") == "hi"
+        assert mgr.exec("aloha") == "aloha"

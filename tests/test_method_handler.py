d9b754d465373213c2e240283c7e533e9f691605
Allan Chain <36528777+AllanChain@users.noreply.github.com>
2020-12-05 20:08:30 +0800

    test: method handlers

---
+++ tests/test_method_handler.py
@@ -0,0 +1,28 @@
+import pytest
+
+from command4bot import CommandsManager
+
+mgr = CommandsManager()
+
+
+class TestMethodHandler:
+    @pytest.fixture(scope="class", autouse=True)
+    def init(self):
+        mgr.setup(self.data)
+        mgr.command(self.tell)
+        mgr.fallback(self.fallback)
+
+    def data(self):
+        return "truth"
+
+    def tell(self, payload, data):
+        return f"tell {payload} {data}"
+
+    def fallback(self, content):
+        return f"unknown {content}"
+
+    def test_command_and_setup(self):
+        assert mgr.exec("tell me") == "tell me truth"
+
+    def test_fallback(self):
+        assert mgr.exec("char") == "unknown char"
